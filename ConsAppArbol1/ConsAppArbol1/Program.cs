d8ebe614c041751904e2b366785419fdbe64592b
Osurse <matyaza@gmail.com>
2022-09-25 21:53:16 -0500

    cv

---
+++ ConsAppArbol1/ConsAppArbol1/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppArbol1
+{
+    public class Nodo
+    {
+        public int Info { set; get; }
+        public Nodo Izq { set; get; }
+        public Nodo Der { set; get; }
+    }
+    public class ArbolBinario
+    {
+        int conteo=0;
+        int cant = 0;
+        public Nodo Raiz { set; get; }
+
+        public ArbolBinario()
+        {
+            Raiz = null;
+        }
+        public void Insertar(int valor)
+        {
+            Nodo nuevo = new Nodo();
+            nuevo.Info = valor;
+            nuevo.Der = null;
+            nuevo.Izq = null;
+            
+
+            //Recorrer el arbol
+            if (Raiz==null)
+            {
+                Raiz = nuevo;
+                conteo++;
+            }
+            else
+            {
+                conteo++;
+                Nodo anterior = null;
+                Nodo reco = Raiz;
+                while (reco != null)
+                {
+                    anterior = reco;
+                    if (valor < reco.Info)
+                    {
+                        reco = reco.Izq;
+                    }
+                    else
+                    {
+                        reco = reco.Der;
+                    }
+                }
+                if (valor < anterior.Info)
+                {
+                    anterior.Izq = nuevo;
+                }
+                else
+                {
+                    anterior.Der = nuevo;
+                }
+            }
+
+        }
+
+        public void Contar()
+        {
+            Console.WriteLine("El numero de nodos totales es: "+conteo);
+        }
+
+        private void ImprimirPre(Nodo reco)
+        {
+            if (reco != null)
+            {
+                Console.Write(reco.Info + " ");
+                ImprimirPre(reco.Izq);
+                ImprimirPre(reco.Der);
+            }
+        }
+
+        public void ImprimirPre()
+        {
+            ImprimirPre(Raiz);
+            Console.WriteLine();
+        }
+
+        private void ImprimirEntre(Nodo reco)
+        {
+            if (reco != null)
+            {
+                ImprimirEntre(reco.Izq);
+                Console.Write(reco.Info + " ");
+                ImprimirEntre(reco.Der);
+            }
+        }
+
+        public void ImprimirEntre()
+        {
+            ImprimirEntre(Raiz);
+            Console.WriteLine();
+        }
+
+        private void CantidadNodosHoja(Nodo reco)
+        {
+            if (reco != null)
+            {
+                if (reco.Izq == null && reco.Der == null)
+                    cant++;
+                CantidadNodosHoja(reco.Izq);
+                CantidadNodosHoja(reco.Der);
+            }
+        }
+
+        public int CantidadNodosHoja()
+        {
+            cant = 0;
+            CantidadNodosHoja(Raiz);
+            return cant;
+        }
+
+        public void MayorValorl()
+        {
+            if (Raiz != null)
+            {
+                Nodo reco = Raiz;
+                while (reco.Der != null)
+                    reco = reco.Der;
+                Console.WriteLine("Mayor valor del árbol:" + reco.Info);
+            }
+        }
+
+        public void BorrarMenor()
+        {
+            if (Raiz != null)
+            {
+                if (Raiz.Izq == null)
+                    Raiz = Raiz.Der;
+                else
+                {
+                    Nodo atras = Raiz;
+                    Nodo reco = Raiz.Izq;
+                    while (reco.Izq != null)
+                    {
+                        atras = reco;
+                        reco = reco.Izq;
+                    }
+                    atras.Izq = reco.Der;
+                }
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinario miArbol = new ArbolBinario();
+            miArbol.Insertar(100);
+            miArbol.Insertar(50);
+            miArbol.Insertar(25);
+            miArbol.Insertar(120);
+            miArbol.Insertar(75);
+            miArbol.Insertar(150);
+            miArbol.Insertar(110);
+            Console.WriteLine("Impresion Pre: ");
+            miArbol.ImprimirPre();
+            miArbol.Contar();
+            miArbol.CantidadNodosHoja();
+            miArbol.MayorValorl();
+            Console.WriteLine("Impresion Entre: ");
+            miArbol.ImprimirEntre();
+            miArbol.BorrarMenor();
+            Console.WriteLine("Impresion Entre con el valor menor borrador: ");
+            miArbol.ImprimirEntre();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsAppBanco/ConsAppBanco/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppBanco
+{
+    public class Cliente
+    {
+        class Nodo
+        {
+            public int info;
+            public Nodo sig;
+        }
+
+        private Nodo raiz, fondo;
+
+        public void Cola()
+        {
+            raiz = null;
+            fondo = null;
+        }
+
+        public bool Vacia()
+        {
+            if (raiz == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void Insertar(int info)
+        {
+            Nodo nuevo;
+            nuevo = new Nodo();
+            nuevo.info = info;
+            nuevo.sig = null;
+            if (Vacia())
+            {
+                raiz = nuevo;
+                fondo = nuevo;
+            }
+            else
+            {
+                fondo.sig = nuevo;
+                fondo = nuevo;
+            }
+        }
+
+        public int Extraer()
+        {
+            if (!Vacia())
+            {
+                int informacion = raiz.info;
+                if (raiz == fondo)
+                {
+                    raiz = null;
+                    fondo = null;
+                }
+                else
+                {
+                    raiz = raiz.sig;
+                }
+                return informacion;
+            }
+            else
+                return 0;
+        }
+
+        
+        public int Cantidad()
+        {
+            int cant = 0;
+            Nodo reco = raiz;
+            while (reco != null)
+            {
+                cant++;
+                reco = reco.sig;
+            }
+            return cant;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ale = new Random();
+            int caja = 0;
+            int llegada = 2 + ale.Next(0, 2);
+            int movimiento = 0;
+            int movimientos = 0;
+            int acu = 0;
+            int salida = 0;
+            int atendidos = 0;
+            Cliente clie = new Cliente();
+            for (int minuto = 0; minuto < 60000; minuto++)
+            {
+                if (llegada == minuto)
+                {
+                    if (caja == 0)
+                    {
+                        caja = 1;
+                        movimientos = 1 + ale.Next(0, 4);//genera los movimientos que se realizaran por el cliente
+                        for (int i = 0; i <= movimientos; i++)//ejecuta el numero de movimientos
+                        {
+                            movimiento = 1 + ale.Next(0, 3);//genera el tipo de movimiento que se realiza
+
+                            switch (movimiento)//suma el timepo tardado por tipo de movimiento
+                            {
+                                case 1://deposito
+                                    acu = acu + 4;
+                                    break;
+                                case 2://retiro
+                                    acu = acu + 5;
+                                    break;
+                                case 3://operacion con cheques
+                                    acu = acu + 7;
+                                    break;
+                                case 4://Transferencias
+                                    acu = acu + 10;
+                                    break;
+                            }
+                            }
+                        salida = salida + acu;
+                        }
+                    }
+                else
+                {
+                    clie.Insertar(llegada);
+                }
+                llegada = llegada +2+ ale.Next(0,2);
+                if (salida==minuto)
+                {
+                    caja = 0;
+                    atendidos++;
+                    if (!clie.Vacia())
+                    {
+                        clie.Extraer();
+                        caja = 1;
+                        movimientos = 1 + ale.Next(0, 4);//genera los movimientos que se realizaran por el cliente
+                        for (int i = 0; i <= movimientos; i++)//ejecuta el numero de movimientos
+                        {
+                            movimiento = 1 + ale.Next(0, 3);//genera el tipo de movimiento que se realiza
+
+                            switch (movimiento)//suma el timepo tardado por tipo de movimiento
+                            {
+                                case 1://deposito
+                                    acu = acu + 4;
+                                    break;
+                                case 2://retiro
+                                    acu = acu + 5;
+                                    break;
+                                case 3://operacion con cheques
+                                    acu = acu + 7;
+                                    break;
+                                case 4://Transferencias
+                                    acu = acu + 10;
+                                    break;
+                            }
+                        }
+                        salida = salida + acu;
+                    }
+                }
+                }
+            Console.WriteLine("Atendidos:"+atendidos);
+            Console.WriteLine("En Espera:"+clie.Cantidad());
+            Console.WriteLine("tiempo de llegada:"+clie.Extraer());
+            Console.ReadLine();
+
+
+
+        }
+        }
+    }
+
+/*Console.WriteLine("¿Cuanto tiempo quieres que dure tu simulacion (máximo 5min.)?");
+int Tiempo = int.Parse(Console.ReadLine());
+Queue<Cliente> Fila = new Queue<Cliente>();
+
+int Timer = DateTime.Now.Minute;
+int Timer2 = 0;
+Cliente cliente = new Cliente(0);
+Fila.Enqueue(cliente);
+int NumeroClientes = 1;
+do
+{
+    cliente = new Cliente(NumeroClientes);
+    Fila.Enqueue(cliente);
+    Timer2 = DateTime.Now.Minute;
+} while (Timer2 - Timer != Tiempo);
+Console.WriteLine("En la fila hay ..." + Fila.Count);
+Console.ReadKey();*/
+
+
